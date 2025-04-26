6cd8b93966c10b70b21fa0ba98e955b07bdaa032
Jere Jacklin <jere.jacklin@gmail.com>
2025-04-26 18:11:42 +0300

    oopsie

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-from huutis import kaytin_oikeaa_salasanaa_tahan
 # MySQL Configuration
 DB_USERNAME = "root"
-DB_PASSWORD = kaytin_oikeaa_salasanaa_tahan
+DB_PASSWORD = "passwd"
 DB_HOST = "localhost"
 DB_NAME = "events_db"
