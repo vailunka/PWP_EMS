ec1f73fad44f05f8ec489af2a995ef1a6c614d58
Jere Jacklin <jere.jacklin@gmail.com>
2025-04-26 18:09:44 +0300

    fix autologin

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+from huutis import kaytin_oikeaa_salasanaa_tahan
 # MySQL Configuration
-DB_USERNAME = "username"
-DB_PASSWORD = "password"
+DB_USERNAME = "root"
+DB_PASSWORD = kaytin_oikeaa_salasanaa_tahan
 DB_HOST = "localhost"
 DB_NAME = "events_db"
