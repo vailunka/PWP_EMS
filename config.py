6ae89c44b2030a69d0daacc859e7cf2ce3895ce7
tomjoki <115992682+tomjoki@users.noreply.github.com>
2025-04-27 22:46:13 +0300

    added deployment yaml, config changes and requirement changes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # MySQL Configuration
-DB_USERNAME = "root"
-DB_PASSWORD = "passwd"
-DB_HOST = "localhost"
-DB_NAME = "events_db"
+import os
+DB_USERNAME = os.getenv("MYSQL_USER")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
+DB_HOST = '172.30.253.12'
+DB_NAME = os.getenv("MYSQL_DATABASE")
